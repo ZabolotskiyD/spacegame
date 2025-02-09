0c6fd5e0e2c645bc4e03d6b5d11c1cba9bf7445c
ZabolotskiyD <Lefort9@gmail.com>
2025-02-09 14:51:50 +0500

    new cdn

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
-import * as THREE from 'three';
-import { RGBELoader } from 'three/examples/jsm/loaders/RGBELoader.js'; // Импортируем RGBELoader
+import * as THREE from "three";
+import { RGBELoader } from "three/addons/loaders/RGBELoader.js";
 
 // Создаем сцену, камеру и рендерер
 const scene = new THREE.Scene();
